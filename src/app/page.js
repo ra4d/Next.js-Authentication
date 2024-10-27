ba425f9d975444a4077eff77473cab43e4ed82fd
rashed <admedrishd6@gmail.com>
2024-10-27 05:01:42 +0300

    create the heaser component

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      home
-      <h2 className={``}>hello world</h2>
+      HOME
+
     </div>
   );
 }
