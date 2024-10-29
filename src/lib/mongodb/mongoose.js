c70e75960d3625d827a833e2f994d1970e95f85c
rashed <admedrishd6@gmail.com>
2024-10-29 05:23:56 +0300

    update mongoconnect

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -10,8 +10,6 @@ const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO, {
       dbName: "next-auth-app",
-      useNewUrlParser: true,
-   		useUnifiedTopology: true
     });
     console.log(`mongoDB connect`);
     initialized = true;
